3eeea39b4f5f72378eee6fb996abfb08c5feff97
Pandu <120022362+pandusp017@users.noreply.github.com>
2023-03-05 18:30:07 +0530

    Deleted Screen functionality

--- Frontend/src/Components/Deleted-Screen/Deleted-Screen.jsx
+++ Frontend/src/Components/Deleted-Screen/Deleted-Screen.jsx
@@ -8,11 +8,11 @@ const DeleteScreen = ({setCom}) => {
 
   return (
     <>
-    <div className='delete-panel'>
+    <div className="delete-panel">
         <div className="delete-blur-display">
             <div className="content">
                 <div className="icon"><RiDeleteBinLine></RiDeleteBinLine> </div>
-                <h4 className='Delete-title'>Delete Contacts</h4>
+                <h4 className="Delete-title">Delete Contacts</h4>
                 <h6 className='question'>Sure you want delete this Contacts ?</h6>
                 <div className="buttons">
                     <button className="cancel" onClick={() => setCom(false)}>Cancel</button>
