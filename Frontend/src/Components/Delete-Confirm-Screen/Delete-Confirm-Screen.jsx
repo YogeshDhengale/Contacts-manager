d481f6278c8a47f9f13968299cd0fa093a7f9b93
Pandu <120022362+pandusp017@users.noreply.github.com>
2023-03-05 22:42:38 +0530

    changes on delete

--- Frontend/src/Components/Delete-Confirm-Screen/Delete-Confirm-Screen.jsx
+++ Frontend/src/Components/Delete-Confirm-Screen/Delete-Confirm-Screen.jsx
@@ -4,12 +4,12 @@ import './Delete-Confirm-Screen.css'
 
 const DeleteConfirmScreen = ({setCom}) => {
     return (
-        <div className='delete-panel'>
+        <div className="delete-panel">
             <div className="delete-blur-display">
                 <div className="content">
                     <div className="icon"><FaCheck></FaCheck> </div>
-                    <h4 className='Delete-title'>Delete Contacts</h4>
-                    <button className='ok' onClick={() => { setCom(false) }}>Ok</button>
+                    <h4 className="Delete-title">Delete Contacts</h4>
+                    <button className="ok" onClick={() => { setCom(false) }}>Ok</button>
                 </div>
             </div>
         </div>
